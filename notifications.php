5a282494fc09c74c855c28275b2a69b656dd4d7b
Charli <frayle023@gmail.com>
2020-11-26 13:50:06 -0700

    First

--- notifications.php
+++ notifications.php
@@ -8,6 +8,8 @@
         $url .= '&'.$key.'='.$value;  
     }
     $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    curl_setopt($ch, CURLOPT_HEADER, 0); 
     $response = curl_exec($ch);
     curl_close($ch);
     echo $response;
