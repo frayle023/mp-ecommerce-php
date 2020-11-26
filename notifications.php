00bee042002de1242186a1d9d550be85bba9e5d5
Charli <frayle023@gmail.com>
2020-11-26 11:03:46 -0700

    First

--- notifications.php
+++ notifications.php
@@ -1,6 +1,4 @@
 <?php
-
-    $token = 'Authorization: token 008b13dbf6f8d087c4595fc16c21fe3f5c2432bd';
     $file = "logs.txt";
     $data = array(
         'sha' => 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
@@ -18,7 +16,6 @@
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        $token,
         "Content-Type: application/json",
         "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/573.36"));
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
