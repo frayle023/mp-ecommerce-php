ee50bb850e7d3a7b3f452cadd50e90d1f84141fd
Charli <frayle023@gmail.com>
2020-11-25 23:39:06 -0700

    First

--- detail.php
+++ detail.php
@@ -191,7 +191,8 @@
 
                                         $preference->save();
                                     ?>
-                                    <a href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
+                                    <button href="<?php echo $preference->init_point; ?>" class="mercadopago-button">Pagar la compra</button>
+                                    
                                 </div>
                             </div>
                         </div>
---
+++ success.php
@@ -0,0 +1,18 @@
+<?php
+
+foreach($_GET as $key => $value)
+{
+    echo "$key : $value"."<br/>";
+}
+
+//collection_id=12414784312
+//collection_status=approved
+//payment_id=12414784312
+//status=approved
+//external_reference=frayle023@gmail.com
+//payment_type=credit_card
+//merchant_order_id=2022707239
+//preference_id=617633181-6bf73eda-28ee-49ac-b5ca-ea88ea4028cd
+//site_id=MLM
+//processing_mode=aggregator
+//merchant_account_id=null
