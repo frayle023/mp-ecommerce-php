c8e2d1a9c00837abdb0b0cf2b26c26cded34a2cc
Charli <frayle023@gmail.com>
2020-11-25 23:58:36 -0700

    First

--- detail.php
+++ detail.php
@@ -146,7 +146,7 @@
                                         $item->id = '1234';
                                         $item->title = $_POST['title'];
                                         $item->description = 'Dispositivo móvil de Tienda e-commerce';
-                                        $path = substr($_POST['img'], 0, 1);
+                                        $path = substr($_POST['img'], 1);
                                         $img = 'https://frayle023-mp-ecommerce-php.heroku.app'.$path; 
                                         $item->picture_url = $img;
                                         $item->quantity = $_POST['unit'];
