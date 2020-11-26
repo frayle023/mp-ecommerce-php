a1fda26056e594db857505f25e71aab2c70b2446
Charli <frayle023@gmail.com>
2020-11-26 11:26:45 -0700

    First

--- notifications.php
+++ notifications.php
@@ -1,6 +1,6 @@
 <?php
-    $username = 'frayle023@gmail.com';
-    $password = 'Elpollo023@';
+    $token = '8f61b8c7da8c7a9d945532b65483ecda186eaf6a';
+    $auth = "Authorization: token $token";
     $file = "logs.txt";
     $data = array(
         'sha' => 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
@@ -18,10 +18,10 @@
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        $auth,
         "Content-Type: application/json",
         "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/573.36"));
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 
     $response = curl_exec($ch);
