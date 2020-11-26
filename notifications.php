cb65494e9a9bf4de1fa1a3cd8d2d98c390eeaf95
Charli <frayle023@gmail.com>
2020-11-26 14:02:12 -0700

    First

--- notifications.php
+++ notifications.php
@@ -2,17 +2,4 @@
 
     MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
     
-    $url = 'https://apps.profecharli.com/lam/add?test=true';
-    foreach($_GET as $key => $value)
-    {
-        $key = str_replace(' ', '', $key);
-        $value = str_replace(' ', '', $value);
-        $url .= '&'.$key.'='.$value;  
-    }
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-    curl_setopt($ch, CURLOPT_HEADER, 0); 
-    $response = curl_exec($ch);
-    curl_close($ch);
-    echo $response;
-    header("HTTP/1.1 200 OK");
+   
