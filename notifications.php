b6beea625c30551c80cc0d58747847ed0e40f400
Charli <frayle023@gmail.com>
2020-11-26 02:22:47 -0700

    First

--- notifications.php
+++ notifications.php
@@ -1,15 +1,38 @@
 <?php
-            $log = "assets/log.txt";
-            $file = fopen($log, "a+");
-            $txt = '';
-            foreach($_POST as $key => $value)
-            {
-                $txt .= $key.' : '.$value.' | ';
-            }          
-            $txt.= ' :end'; 
-            fwrite($file, $txt);
-            fclose($file);
-            header("HTTP/1.1 200 OK");
+
+//set date time if not already done elsewhere
+date_default_timezone_set('America/Los_Angeles');
+
+//set date/time for entry
+$time_stamp = date('m\-d\-Y\-h:iA');
+
+//set file name and location
+$log = "assets/logs/log.txt";
+
+//open or create log.txt
+//[see docs for more modes](http://php.net/manual/en/function.fopen.php)
+$fp = fopen($log,"a+");
+
+//append text and time stamp to log.txt (the double \n's are to give readable space inbetween entries.) 
+fwrite($fp, $time_stamp . "\n" . "hello world" . "\n\n");
+
+//close log file
+fclose($fp);
+
+//You can also use unlink() to delete file if needed
+
+
+            // $log = "assets/log.txt";
+            // $file = fopen($log, "a+");
+            // $txt = '';
+            // foreach($_POST as $key => $value)
+            // {
+            //     $txt .= $key.' : '.$value.' | ';
+            // }          
+            // $txt.= ' :end'; 
+            // fwrite($file, $txt);
+            // fclose($file);
+            // header("HTTP/1.1 200 OK");
 
 
 
