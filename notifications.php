35ac7a488bf3fb6b77c2af362efd913bb343094c
Charli <frayle023@gmail.com>
2020-11-26 10:51:13 -0700

    First

--- notifications.php
+++ notifications.php
@@ -1,8 +1,17 @@
 <?php
 
     $token = '008b13dbf6f8d087c4595fc16c21fe3f5c2432bd';
-    $data = "Texto de prueba";
-    $data = base64_encode($data);
+    $file = "logs.txt";
+    $data = array(
+        'sha' => 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
+        'message' => 'PHP Commit',
+        'content' => base64_encode($file),
+        'commiter' => array(
+            'name' => 'Charli',
+            'email' => 'frayle023@gmail.com'
+        )
+    );
+    $data_string = json_encode($data);
 
     $curl_url = 'https://api.github.com/repos/frayle023/mp-ecommerce-php/contents/logs.txt';
     $ch = curl_init($curl_url);
@@ -13,7 +22,7 @@
         "Authorization: token $token",
         "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/573.36"));
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 
     $response = curl_exec($ch);
     curl_close($ch);
