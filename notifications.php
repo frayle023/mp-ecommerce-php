fcadbc424ff7e62da0aaf2598d124b48f336ed5f
Charli <frayle023@gmail.com>
2020-11-26 13:29:58 -0700

    First

--- notifications.php
+++ notifications.php
@@ -1,6 +1,6 @@
 <?php
 
-MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
 
 switch($_POST["type"]) {
     case "payment":
