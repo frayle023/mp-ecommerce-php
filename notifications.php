fd67bfb207c8f1406f28497df545bb0fadc49652
Charli <frayle023@gmail.com>
2020-11-26 16:38:18 -0700

    First

--- notifications.php
+++ notifications.php
@@ -5,50 +5,21 @@ require __DIR__ .  '/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
-    switch($_POST["type"]) {
-        case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            $txt = '';
-            foreach($_POST as $key => $value)
-            {
-                $txt .= $key.' : '.$value. ' | ';
-            }
-            file_put_contents('logs.txt', $txt);
-            header("HTTP/1.1 200 OK"); 
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-            $txt = '';
-            foreach($_POST as $key => $value)
-            {
-                $txt .= $key.' : '.$value. ' | ';
-            }
-            file_put_contents('logs.txt', $txt);
-            header("HTTP/1.1 200 OK"); 
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            $txt = '';
-            foreach($_POST as $key => $value)
-            {
-                $txt .= $key.' : '.$value. ' | ';
-            }
-            file_put_contents('logs.txt', $txt);
-            header("HTTP/1.1 200 OK"); 
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            $txt = '';
-            foreach($_POST as $key => $value)
-            {
-                $txt .= $key.' : '.$value. ' | ';
-            }
-            file_put_contents('logs.txt', $txt);
-            header("HTTP/1.1 200 OK"); 
-            break;
-    }
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
 
-header("HTTP/1.1 200 OK"); 
+    //echo var_dump($data);
+
+    // $txt = '';
+    // foreach($_POST as $key => $value)
+    // {
+    //     $txt .= $key.' : '.$value. ' | ';
+    // }
+    $previo = file_get_contents('logs.txt');
+    $nuevo = $previo.PHP_EOL.$json;
+    file_put_contents('logs.txt', $nuevo);
+    header("HTTP/1.1 200 OK"); 
+
 ?>
     
    
