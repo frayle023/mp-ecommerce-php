ecb5d60febb9bc7d50509156551612cc002d440b
Charli <frayle023@gmail.com>
2020-11-27 12:58:21 -0700

    First

--- notifications.php
+++ notifications.php
@@ -6,19 +6,11 @@ MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c749498
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
     $json = file_get_contents('php://input');
-    $data = json_decode($json);
-    $code = json_encode($json);
+    //$data = json_decode($json);
+    //$code = json_encode($json);
     
-
-    //echo var_dump($data);
-
-    // $txt = '';
-    // foreach($_POST as $key => $value)
-    // {
-    //     $txt .= $key.' : '.$value. ' | ';
-    // }
     $previo = file_get_contents('logs.txt');
-    $nuevo = $previo.PHP_EOL.$code;
+    $nuevo = $previo.PHP_EOL.$json;
     file_put_contents('logs.txt', $nuevo);
     header("HTTP/1.1 200 OK"); 
 
